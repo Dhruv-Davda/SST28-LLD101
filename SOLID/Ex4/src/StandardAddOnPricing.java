72179b60d6e93d42ec98b26184df271d201bd748
Dhruv Davda <dhruvdavda500@gmail.com>
2026-02-23 03:44:39 +0530

    Exercise 4 Done

---
+++ SOLID/Ex4/src/StandardAddOnPricing.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class StandardAddOnPricing implements AddOnPricing {
+    private static final Map<AddOn, Double> PRICES = Map.of(
+        AddOn.MESS, 1000.0,
+        AddOn.LAUNDRY, 500.0,
+        AddOn.GYM, 300.0
+    );
+
+    public Money priceFor(AddOn addOn) {
+        return new Money(PRICES.getOrDefault(addOn, 0.0));
+    }
+}
